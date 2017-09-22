432748bd64d944889eded09829ba0150ef194db3
amanndajosefiina <amanndavainikka@gmail.com>
2017-09-22 14:14:20 +0300

    task-2 toimii

---
+++ Conditional-statements/task-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // define instructions
+            Console.WriteLine("Ohjelma tarkistaa onko syötetty luku parillinen vai pariton");
+            bool isNumber;
+            string userInput = Console.ReadLine();
+            int evaluatedNumber;
+            isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+            // program logic
+            if (evaluatedNumber % 2 == 0)
+            {
+                Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on parillinen");
+            }
+            else
+            {
+                Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on pariton");
+            }
+            Console.ReadKey();
+        }
+    }
+}
