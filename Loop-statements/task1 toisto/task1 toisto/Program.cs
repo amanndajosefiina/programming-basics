2aae47fb16f3e1dd9bf898562ec854a871758223
amanndajosefiina <amanndavainikka@gmail.com>
2017-10-19 21:14:54 +0300

    loop toistoja

---
+++ Loop-statements/task1 toisto/task1 toisto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task1_toisto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret.");
+
+            int luku = 1;
+            double neliö;
+
+            while (luku <= 10)
+            {
+                neliö = Math.Round(Math.Sqrt(luku), 2);
+                Console.WriteLine($"Luku {luku} neliöjuuri {neliö}");
+                luku = luku + 1;                
+            }
+
+
+            Console.ReadKey();                   
+        }
+    }
+}
---
+++ Loop-statements/task1 toisto/task2 toisto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task2_toisto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa numeroiden 1-9 kertotaulut.");
+
+            int luku = 1;
+            int kerto;
+            int kertoja;
+
+            while (luku <= 9)
+            {
+                while (kertoja <= 10)
+                {
+
+
+                }
+
+                luku = luku + 1;
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
