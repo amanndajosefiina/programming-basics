d322f37ac3a6714dc68053c0ceacbaab7c47a8c7
amanndajosefiina <amanndavainikka@gmail.com>
2017-10-20 10:40:21 +0300

    toisto3 tehtävä valmis

--- Loop-statements/task1 toisto/task2 toisto/Program.cs
+++ Loop-statements/task1 toisto/task2 toisto/Program.cs
@@ -9,18 +9,20 @@ namespace task2_toisto
             Console.WriteLine("Ohjelma tulostaa numeroiden 1-9 kertotaulut.");
 
             int luku = 1;
-            int kerto;
-            int kertoja;
+            int summa;
+            int kertoja = 1;
 
             while (luku <= 9)
             {
                 while (kertoja <= 10)
                 {
-
-
+                    summa = luku * kertoja;
+                    Console.WriteLine($"{luku}*{kertoja} = {summa}");
+                    kertoja = kertoja + 1;
                 }
 
                 luku = luku + 1;
+                kertoja = 1;
             }
 
 
---
+++ Loop-statements/task1 toisto/task3 toisto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task3_toisto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine("Ohjelma tulostaa 20 satunnaista lukua.");
+
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write($"Rivi {i}: ");
+                for (int y = 1; y <= 5; y++)
+                {
+                    if (y < 5)
+                        Console.Write($"{rnd.Next(50)},");
+                    else
+                        Console.Write($"{rnd.Next(50)} \n");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
