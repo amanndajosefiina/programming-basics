b5b819be53949832e91bb0eca5121792840da929
amanndajosefiina <amanndavainikka@gmail.com>
2017-10-23 13:36:04 +0300

    funktio task1

---
+++ Functions/funktio task1/funktio task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace funktio_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Funktio palauttaa annetun määrän tähtiä, jotka pääohjelma tulostaa näytölle. Anna luku:  ");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine($"{GetStars(userInput)}");
+
+            Console.ReadKey();
+        }
+
+        static string GetStars(string numberOfStars)
+        {
+            int number = int.Parse(numberOfStars);
+            string printStars = "";
+
+            if (number <= 0)
+                return "Antamasi luku on virheellinen. Syötä positiivia lukuja.";
+
+            for (int i = 0; i < number; i++)
+            {
+                printStars += "*"; //printStars = printStars + "*"
+            }
+            return printStars;
+
+        }
+
+    }
+}
