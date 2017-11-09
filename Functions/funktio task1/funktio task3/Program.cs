dec1f1d2b20ba120c382a8d6e4eeda157250ddfe
amanndajosefiina <amanndavainikka@gmail.com>
2017-11-09 09:18:25 +0200

    functions

--- Functions/funktio task1/funktio task3/Program.cs
+++ Functions/funktio task1/funktio task3/Program.cs
@@ -6,13 +6,40 @@ namespace funktio_task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anna luku väliltä 1-10: ");
+            Console.WriteLine("Anna luku väliltä 10-20: ");
+            int lower = 10;
+            int upper = 20;
 
-            
+            Console.WriteLine($"Syötit oikean luvun {retNumber(lower, upper)}");
 
+            Console.ReadKey();
 
-
-            retNumber = numberFromRange(lowerBound, upperBound);
         }
+
+        static int retNumber(int lower, int upper)
+
+        {
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            do
+            {
+
+                if (number < 10 || number > 20)
+                {
+                    Console.WriteLine("Syöttämäsi luku on virheellinen. Syötä uusi luku.");
+                    userInput = Console.ReadLine();
+                    number = int.Parse(userInput);
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (true);
+
+            return number;
+
+        }       
     }
 }
