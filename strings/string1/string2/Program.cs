85639aacdb9e477ac695ddf6b466fa67d225e653
amanndajosefiina <amanndavainikka@gmail.com>
2017-11-24 11:24:43 +0200

    strings

---
+++ strings/string1/string2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace string1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ohjelma ilmoittaa käyttäjän syöttämästä syötteestä merkkien lukumäärän. \nKirjoita kiitos: ");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine($"Syötteessä oli {userInput.Length} merkkiä.");
+
+            Console.ReadKey();
+        }
+    }
+}
